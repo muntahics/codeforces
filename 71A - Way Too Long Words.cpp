db6eb59c7ffd53ee2e371b075550449f69f08e4f
muntahics <muntahi05@outlook.com>
2026-06-06 01:38:09 +0100

    this is just the beginning

---
+++ 71A - Way Too Long Words.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    string str;
+    for(i=0;i<n;i++)
+    {
+        cin>>str;
+
+        if(str.length()>10)
+        {
+            cout<<str[0]<<str.length()-2<<str[str.length()-1]<<endl;
+
+        }
+        else
+            cout<<str<<endl;
+    }
+
+    return 0;
+
+}
